f4c7a608060a6d307cb9b6da9a9930efe76124fa
ellikamishra <55554508+ellikamishra@users.noreply.github.com>
2020-03-15 12:25:53 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import "./styles.css";
+
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
